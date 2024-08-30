81563b9a6f22bf6ed2634d547cb546d2eec5ba04
CicekHasa <hasancicek2001ist@gmail.com>
2024-08-30 17:15:38 +0300

    Tema Dosyası eklendi.
    Katmanların içine eklenmesi gereken klasörler eklendi.
    
    Entity katmanındaki concrete klasöründe Category ve Course sınıfları oluşturularak propertyler eklendi.

--- Edukator.PresentationLayer/Edukator.EntityLayer/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Edukator.EntityLayer
-{
-    public class Class1
-    {
-    }
-}
---
+++ Edukator.PresentationLayer/Edukator.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edukator.EntityLayer.Concrete
+{
+    public class Category
+    {
+        //değişken
+        //int x;
+
+        //Property
+        //class ile aynı isimde id oluşturulursa o değişkeni tablonun birincil anahtarı olarak ve identity'si de 1 er kez artacak şekilde algılar.
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }    
+    }
+}

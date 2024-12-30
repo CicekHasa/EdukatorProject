aefbf7ecfe0d4dd1ece569233c4f07797382a397
Hasan Çiçek <hasancicek2001ist@gmail.com>
2024-12-31 02:06:28 +0300

    31.12.2024 02:03
    Admin tarafında Öğrenciler alanında AspNetUsers tablosundaki verileri listelemek için 'Users' controller oluşturuldu.
    
    Admin tarafında AspNetRoles tablosundaki rolleri göstermek için de 'Roles' controller'ı oluşturuldu ve CRUD işlemleri yapıldı.
    
    Admin sayfasında kullanıclara roller atamak için 'RoleAssign' controller'ı oluşturuldu. Kullanıcıları listelemek için view kısmı oluşturuldu ancak rol ataması yapılacak sayfanın dizayn ve kodu yazılacak.

---
+++ Edukator.PresentationLayer/Edukator.PresentationLayer/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Edukator.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Edukator.PresentationLayer.Controllers
+{
+    public class UsersController : Controller
+    {
+        //Burada AppUser Identity den gelen AspNetUser tablosunu özelleştirdiğim için kullanıyorum. Ancak Identity nin verdiği AspNetUser tablosunu özelleştirmeseydim UserManager içerisinde Identity nin verdiği 'IdentityUser' sınıfını eklemeliydim!
+        private readonly UserManager<AppUser> _userManager;
+
+        public UsersController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _userManager.Users.ToList();
+            return View(values);
+        }
+    }
+}

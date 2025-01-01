b840cd5186a40ecd7c08ca10710bc806f81d12b0
Hasan Çiçek <hasancicek2001ist@gmail.com>
2025-01-02 01:19:45 +0300

    02.01.2025 01:16
    Admin panelinde kullanıcıya özel atanan rolleri ekranda listeledik.
    
    Kullanıcıda olan roller checkbox ta işaretli iken kullanıcıda bulunmayan roller işaretli olmayarak ekranda gözüktü.
    
    Kullanıcıya eklemek istediğimiz yeni rolleri ilgili rolün kutusuna basarak ve bulunan rollerinden de alınmasını istediğimiz rollerin kutularına da basarak rol ataması yaptığımızda o kullanıcıya ait roller 'AspNetUserRoles' tablosunda güncellenmiş oldu
    
    TempData mekanizması öğrenildi ve RoleAssignController da kullanıldı.

--- Edukator.PresentationLayer/Edukator.PresentationLayer/Controllers/RoleAssignController.cs
+++ Edukator.PresentationLayer/Edukator.PresentationLayer/Controllers/RoleAssignController.cs
@@ -1,6 +1,8 @@
 ﻿using Edukator.EntityLayer.Concrete;
+using Edukator.PresentationLayer.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -26,10 +28,40 @@ namespace Edukator.PresentationLayer.Controllers
         [HttpGet]
         public async Task<IActionResult> UserRoleAssign(int id)
         {
-            var user = _userManager.Users.FirstOrDefault(x => x.Id == id);
-            var roles=_roleManager.Roles.ToList();
-            var userRoles=await _userManager.GetRolesAsync(user);
-            return View();
+            var user = _userManager.Users.FirstOrDefault(x => x.Id == id);//seçilen kişinin id sine göre kullanıcıyı getirir.
+            TempData["userid"] = user.Id;
+            var roles = _roleManager.Roles.ToList();//Tüm rolleri getirir.
+            var userRoles = await _userManager.GetRolesAsync(user);//Seçilen kullanıcının hangi rolleri olduğunu getirir.
+            List<RoleAssignViewModel> roleAssignViewModel = new List<RoleAssignViewModel>();
+
+            foreach (var item in roles)
+            {
+                RoleAssignViewModel model = new RoleAssignViewModel();
+                model.RoleID = item.Id;
+                model.RoleName = item.Name;
+                model.RoleExist = userRoles.Contains(item.Name);
+                roleAssignViewModel.Add(model);
+            }
+            return View(roleAssignViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserRoleAssign(List<RoleAssignViewModel> model)
+        {
+            var userId = (int)TempData["userId"];
+            var user=_userManager.Users.FirstOrDefault(x=>x.Id== userId);//id ye ait user'ı bulur.
+            foreach (var item in model)
+            {
+                if (item.RoleExist)
+                {
+                    await _userManager.AddToRoleAsync(user, item.RoleName);//Yeni bir role ekler.
+                }
+                else
+                {
+                    await _userManager.RemoveFromRoleAsync(user, item.RoleName);//Var olan role alındıysa kullanıcıdan siler.
+                }
+            }
+            return RedirectToAction("Index");
         }
     }
 }

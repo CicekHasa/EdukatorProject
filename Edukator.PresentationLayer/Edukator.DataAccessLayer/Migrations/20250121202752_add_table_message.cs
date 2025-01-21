e83697f71dc76cc56a7c17d109540f4074073e4e
Hasan Çiçek <hasancicek2001ist@gmail.com>
2025-01-21 23:54:10 +0300

    21.01.2024 23:51
    Iletişim sayfasında soru iletmek için olan form kısmı için 'Message' adında entity oluşturuldu.
    
    Tüm katmanlardaki klasörler de sınıf ve interfaceleri oluşturuldu.
    
    Iletişim sınıfındaki form doldurulup mesaj göndeirldiğinde 'Messages' tablosuna bilgiler kayıt edilmekte olup yapı dinamik hale getirildi.

---
+++ Edukator.PresentationLayer/Edukator.DataAccessLayer/Migrations/20250121202752_add_table_message.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Edukator.DataAccessLayer.Migrations
+{
+    public partial class add_table_message : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    MessageID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Subject = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Mail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TextMessage = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.MessageID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+        }
+    }
+}
--- Edukator.PresentationLayer/Edukator.PresentationLayer/Controllers/ContactController.cs
+++ Edukator.PresentationLayer/Edukator.PresentationLayer/Controllers/ContactController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Edukator.BusinessLayer.Abstract;
+using Edukator.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Edukator.PresentationLayer.Controllers
 {
     public class ContactController : Controller
     {
+
+        private readonly IMessageService _messageService;
+
+        public ContactController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -13,6 +23,13 @@ namespace Edukator.PresentationLayer.Controllers
         public PartialViewResult ContactSendMessagePartial()
         {
             return PartialView();
+        }
+
+        [HttpPost]
+        public IActionResult ContactSendMessagePartial(Message message)
+        {
+            _messageService.TInsert(message);
+            return RedirectToAction("Index");
         }
     }
 }
